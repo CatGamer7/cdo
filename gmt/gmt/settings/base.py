ccce98a32026aa209189f363d4d03c65dfeaee3c
CatGamer7 <arthur.zayda@gmail.com>
2024-12-07 03:10:37 +0700

    fix: k8s deployment host fix

--- gmt/gmt/settings/base.py
+++ gmt/gmt/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*bhay7u-m_zstu9mqydig$wl^mco_21gspx7rl#l3x8b5!wpin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
